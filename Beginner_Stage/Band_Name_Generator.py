12355aafe5c73fc5f028dcf775e296ec0a3127c5
Victor Nice <victornice550@gmail.com>
2023-03-22 06:01:53 +0100

    Create Band Name Generator Program

---
+++ Beginner_Stage/Band_Name_Generator.py
@@ -0,0 +1,7 @@
+"""
+This program generate a name for your amazing band.
+"""
+city = input("Enter the city you grew up from:\n")
+pet = input("Enter the name of your favourite pet:\n")
+
+print("Your new band name is: {} {}".format(city, pet))
