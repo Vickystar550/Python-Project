f0d21beef677a7cd26c76cb20b820b7955d47040
Victor Nice <victornice550@gmail.com>
2023-03-25 02:52:21 +0100

    Modify Rock_Paper_Scissor.py

--- Beginner_Stage/Day_4/Rock_Paper_Scissor.py
+++ Beginner_Stage/Day_4/Rock_Paper_Scissor.py
@@ -27,9 +27,9 @@ paper = """
       (____)
 ---.__(___)"""
 
-print("hello world")
 print(rock)
 print(paper)
+print(scissor)
 
 user_choice = int(input("What do you choose. Type 0 for Rock, 1 for Paper, and 2 for Scissor\n"))
 
@@ -37,30 +37,30 @@ computer_choice = random.randint(0, 2)
 print(f"Computer Choice: {computer_choice}")
 
 # Checking for winning:
-# If user choose 0 -- Rock:
+# If user choice = 0 (Rock):
 if user_choice == 0:
     if computer_choice == 1:
-        print("You choose Rock, computer choose Paper. Paper wraps Rock. You Lose!!! 😰😰😰")
+        print("You chose Rock, computer chose Paper. Paper wraps Rock. You Lose!!! 😰😰😰")
     elif computer_choice == 2:
-        print("You choose Rock, computer choose Scissor. Rock smash Scissor. You Win!!! 😍😍😍")
+        print("You chose Rock, computer chose Scissor. Rock smash Scissor. You Win!!! 😍😍😍")
     else:
-        print("You choose Rock, computer choose Rock. It is\'s a tie!!! 😳😳😳")
-# If user choose 1 -- Paper:
+        print("You chose Rock, computer chose Rock. It is\'s a tie!!! 😳😳😳")
+# If user choice = 1 (Paper):
 elif user_choice == 1:
     if computer_choice == 0:
-        print("You choose Paper, computer choose Rock. Paper wraps Rock. You Win!!! 😍😍😍")
+        print("You chose Paper, computer chose Rock. Paper wraps Rock. You Win!!! 😍😍😍")
     elif computer_choice == 2:
-        print("You choose Paper, computer choose Scissor. Scissor cut Paper. You Lose!!! 😰😰😰")
+        print("You chose Paper, computer chose Scissor. Scissor cut Paper. You Lose!!! 😰😰😰")
     else:
-        print("You choose Paper, computer choose Paper. It is\'s a tie!!! 😳😳😳")
-# If user choose 2 -- Scissor:
+        print("You chose Paper, computer chose Paper. It is\'s a tie!!! 😳😳😳")
+# If user chose: 2 (Scissor):
 elif user_choice == 2:
     if computer_choice == 0:
-        print("You choose Scissor, computer choose Rock. Rock smash Scissor. You Lose!!! 😰😰😰")
+        print("You chose Scissor, computer chose Rock. Rock smash Scissor. You Lose!!! 😰😰😰")
     elif computer_choice == 1:
-        print("You choose Scissor, computer choose Paper. Scissor cut Paper. You Win!!! 😍😍😍")
+        print("You chose Scissor, computer chose Paper. Scissor cut Paper. You Win!!! 😍😍😍")
     else:
-        print("You choose Scissor, computer choose Scissor. It is\'s a tie!!! 😳😳😳")
-# If user choose something else
+        print("You chose Scissor, computer chose Scissor. It is\'s a tie!!! 😳😳😳")
+# If user chooses something else
 else:
     print("Invalid input, you Lose!!! 🙄🙄🙄")
