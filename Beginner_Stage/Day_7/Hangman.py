b93142ea34a8347309a700bad3e23fde27224ad1
Victor Nice <victornice550@gmail.com>
2023-03-30 23:41:00 +0100

    Day 7 completed

--- Beginner_Stage/Day_7/Hangman.py
+++ Beginner_Stage/Day_7/Hangman.py
@@ -1,11 +1,42 @@
 """" This is a Hangman game """
 
 import random
+from hangman_words import words
+import hangman_art
 
-word_list = ["aardvark", "baboon", "camel"]
+# VARIABLE SETTING: ####
+# 1. Choose a random word from list of words:
+chosen_word = random.choice(words.split())
 
-chosen_word = random.choice(word_list)
+# 2. Create a blank variable with an underscore that will be filled with user's input:
+blank_list = []
+for i in range(len(chosen_word)):
+    blank_list += "_"
 
-guess = input("Guess a letter in Hangman\'s word:\n").lower()
+# START THE GAME: ####
+print(hangman_art.logo)
+print(f"Guess a {len(chosen_word)}-lettered word in the Hangman's Dictionary")
+lives = 7
+end_of_game = False
+while not end_of_game:
+    # Ask for user input
+    guess_letter = input("Enter a letter:\n").lower()
 
-# for i in chosen_word:
+    # Loop and check if guess_letter is in the chosen word, replace blank_list with guess_letter:
+    for i in range(len(chosen_word)):
+        if guess_letter == chosen_word[i]:
+            blank_list[i] = guess_letter
+
+    # When the guess letter isn't in the chosen word:
+    if guess_letter not in chosen_word:
+        print(f"You have entered \'{guess_letter}\' which is not a letter in the chosen word. You lose a life")
+        lives -= 1
+        print(hangman_art.stages[lives])
+        if lives == 0:
+            end_of_game = True
+            print("You have lost!!!😭😭😭")
+
+    # check if user has filled all the blank spaces in blank_list:
+    if "_" not in blank_list:
+        end_of_game = True
+        print("You win!!!🥰🥰🥰")
--- Beginner_Stage/Day_7/README.md
+++ Beginner_Stage/Day_7/README.md
@@ -1,1 +1,25 @@
 # Day 7 of 100 days
+
+At the end of today, we will be developing a Hangman game
+
+Hangman is a guessing for two or more players. One players thinks of a
+word, phrase or sentences...
+
+For every wrong letter you entered, you end up taking a life...
+
+Steps followed are:
+1. Set a list of words
+2. make a random choice out of it
+3. note the numbers of characters in word
+4. ask the for user input equivalent to the number of character in word;
+    create an empty variable to store the user's response.
+5. loop through words to see if the character entered by user is the same
+    and match with the one in word -- at the corresponding location
+6. if it matches, create a variable to store the user's input
+7. if it doesn't, draw a hangman at every point the user input a wrong
+    character.
+8. Keep asking for user input till all the blank spaces are filled
+9. ...
+
+This directory contains files used as modules in the main program(Hangman.py)
+They are imported in the main file.
