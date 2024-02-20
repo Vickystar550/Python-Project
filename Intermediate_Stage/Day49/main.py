35e224216b6bd56578d42095fe68a6897a70b019
victornice <victornice550@gmail.com>
2024-02-20 11:44:12 +0100

    To Be Done Later: Tinder and Linkedin Job Automation

---
+++ Intermediate_Stage/Day49/main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+# will come back when i am done with 100 days
+
+# i really need to apply to jobs on l....
